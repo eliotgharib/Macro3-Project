e38e65278e397ced3ca9dcdb420adc0d53f5bac7
EliotGharib <eliotgharib@hotmail.com>
2026-05-13 14:14:17 +0200

    Initial project structure

---
+++ code/code.R
@@ -0,0 +1,19 @@
+#SETUP
+
+#Install packages
+install.packages(c("WDI", 
+"countrycode", 
+"ggplot2", 
+"dplyr", 
+"tidyr", 
+"plm",
+"stargazer"))
+
+#Load packages
+library(WDI)
+library(countrycode)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stargazer)
+library(plm)
